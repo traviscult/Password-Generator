22415adb22994ced262006ef4ca3132db5b3b852
traviscult <traviscult@gmail.com>
2020-03-09 12:12:22 -0400

    cleaned up code

--- Develop/script.js
+++ Develop/script.js
@@ -1,50 +1,61 @@
-// Password character count and selection is responsive 
-const charRange = document.getElementById ('charRange')
-const charNumber = document.getElementById ('charNumber')
-const includeUppercaseElement = document.getElementById ('includeUppercase')
-const includeNumbersElement = document.getElementById ('includeNumbers')
-const includeSymbolsElement = document.getElementById ('includeSymbols')
-const form = document.getElementById ('passwordGeneratorForm')
+// Assignment Code
+var characterAmountRange = document.getElementById('characterRange')
+var characterAmountNumber = document.getElementById('characterNumber')
+var includeUppercaseElement = document.getElementById('includeUppercase')
+var includeNumbersElement = document.getElementById('includeNumbers')
+var includeSymbolsElement = document.getElementById('includeSymbols')
+var form = document.getElementById('passwordGeneratorForm')
+var securepassword = document.getElementById('securepassword')
 
-charNumber.addEventListener('input', syncCharacterAmount)
-charRange.addEventListener('input', syncCharacterAmount)
+var UPPERCASE_CHAR_CODES = arrayFromLowToHigh(65, 90)
+var LOWERCASE_CHAR_CODES = arrayFromLowToHigh(97, 122)
+var NUMBER_CHAR_CODES = arrayFromLowToHigh(48, 57)
+var SYMBOL_CHAR_CODES = arrayFromLowToHigh(33, 47).concat(
+  arrayFromLowToHigh(58, 64)
+).concat(
+  arrayFromLowToHigh(91, 96)
+).concat(
+  arrayFromLowToHigh(123, 126)
+)
+
+characterAmountNumber.addEventListener('input', syncCharacterAmount)
+characterAmountRange.addEventListener('input', syncCharacterAmount)
 
 // Stop page from refreshing and pass selection to btn
-form.addEventListener ('submit', e =>{
+form.addEventListener('submit', e => {
   e.preventDefault()
-  const characterAmount = charNumber.value
-  const includeUppercase = includeUppercaseElement.checked
-  const includeNumbers = includeNumbersElement.checked
-  const includeSymbols = includeSymbolsElement.checked
-  const password = generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols)
-}) 
+  var characterAmount = characterAmountNumber.value
+  var includeUppercase = includeUppercaseElement.checked
+  var includeNumbers = includeNumbersElement.checked
+  var includeSymbols = includeSymbolsElement.checked
+  var password = generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols)
+  securepassword.innerText = password
+})
 
-// code to generate the password
-function generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols){
-  String.fromCharCode(65)
+function generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols) {
+  let charCodes = LOWERCASE_CHAR_CODES
+  if (includeUppercase) charCodes = charCodes.concat(UPPERCASE_CHAR_CODES)
+  if (includeSymbols) charCodes = charCodes.concat(SYMBOL_CHAR_CODES)
+  if (includeNumbers) charCodes = charCodes.concat(NUMBER_CHAR_CODES)
+  
+  var passwordCharacters = []
+  for (let i = 0; i < characterAmount; i++) {
+    var characterCode = charCodes[Math.floor(Math.random() * charCodes.length)]
+    passwordCharacters.push(String.fromCharCode(characterCode))
+  }
+  return passwordCharacters.join('')
 }
 
-
-function syncCharacterAmount(e){
-  const value = e.target.value
-  charNumber.value = value
-  charRange.value = value
+function arrayFromLowToHigh(low, high) {
+  var array = []
+  for (let i = low; i <= high; i++) {
+    array.push(i)
+  }
+  return array
 }
 
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+function syncCharacterAmount(e) {
+  var value = e.target.value
+  characterAmountNumber.value = value
+  characterAmountRange.value = value
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-
